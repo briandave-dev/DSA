145599e6674250d0e1c112d5ff35838a5f89c259
briandave-dev <briandave.dev@gmail.com>
2024-09-24 13:47:12 +0200

    comments added to the optimised missing number function

--- blind75/missingNumber.ts
+++ blind75/missingNumber.ts
@@ -1,7 +1,8 @@
-//In this solution i just loop through numbers from 0 to n, check if the
-//number is in the array then return the number if it is not in it
+// In this solution i just loop through numbers from 0 to n, check if the
+// number is in the array then return the number if it is not in it
 
-//It as a time and space complexity of O(n^2)
+// It as a time complexity of O(n^2) because of the nested loop caused by
+// includes function
 
 function missingNumber(nums: number[]): any {
     for(let i:number = 0; i <= nums.length; i++){
@@ -12,11 +13,19 @@ function missingNumber(nums: number[]): any {
     return
 };
 
+// This solution follows the logic that missing number is going to be 
+// the diffenre between the sum of first n terms and the actual sum of
+// all the numbers in our array of numbers
+
+// It has a time complexity if O(n) because the reduce function loops 
+// the array once to add upp each element
+
 function optimisedMissingNumber(nums : number[]){
     const n = nums.length;
     const expectedSum = ((n*(n+1))/2)
     const actualSum = nums.reduce((acc, val) => acc + val, 0)
-    return expectedSum - actualSum
+    const missingNumber : number =  (expectedSum - actualSum)
+    return missingNumber
 }
 
 console.log(optimisedMissingNumber([9,6,4,2,3,5,7,0,1]))
