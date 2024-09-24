cd9f526eb49015197fc5891c2788445c00ab36e3
briandave-dev <briandave.dev@gmail.com>
2024-09-24 23:20:57 +0200

    stock market max profit done

--- blind75/stock.ts
+++ blind75/stock.ts
@@ -1,35 +1,32 @@
-// function maxProfit(prices: number[]): number {
-//     let complement = 0;
-//     for(let i = 0; i < prices.length; i++){
-//         for(let j = i+1; j <= prices.length; j++){
-//             if(prices[j] - prices[i] > complement){
-//                 complement = prices[j] - prices[i];
-//             }
-//         }
-//     }
-//     return complement;
-// };
-
-// console.log(maxProfit([7,4,5,3,6,1]))
+// Here i have a nested loop to compare each element with all the
+// ones next to it one after the other
 
 function maxProfit(prices: number[]): number {
-    let minPrice = Infinity; // Initialize minPrice to a very large number
-    let maxProfit = 0; // Initialize maxProfit to 0
-
-    // Iterate over the prices array
-    for (let i = 0; i < prices.length; i++) {
-        // Check if the current price is less than minPrice
-        if (prices[i] < minPrice) {
-            minPrice = prices[i]; // Update minPrice to the current price
-        } 
-        // Check if the current profit (current price - minPrice) is greater than maxProfit
-        else if (prices[i] - minPrice > maxProfit) {
-            maxProfit = prices[i] - minPrice; // Update maxProfit to the new maximum profit
+    let complement = 0;
+    for(let i = 0; i < prices.length; i++){
+        for(let j = i+1; j <= prices.length; j++){
+            if(prices[j] - prices[i] > complement){
+                complement = prices[j] - prices[i];
+            }
         }
     }
+    return complement;
+};
 
-    return maxProfit; // Return the maximum profit found
+function optimisedMaxProfit(prices: number[]): number {
+    let maxProfit = 0;
+    let minValue = Infinity;
+
+    for(let i: number = 0; i < prices.length; i++){
+        if(minValue > prices[i]){
+            minValue = prices[i];
+        } else if(prices[i] - minValue > maxProfit){
+            maxProfit = prices[i] - minValue;
+        } 
+    }
+    return maxProfit;
 }
 
-console.log(maxProfit([7, 1, 5, 3, 6, 4])); // Output: 5
+
+console.log(optimisedMaxProfit([7, 1, 5, 3, 6, 4])); // Output: 5
 
