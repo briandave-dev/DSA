31f3b05a266bb4f74110481500c14bb4c2d76e6c
briandave-dev <briandave.dev@gmail.com>
2024-10-03 19:23:58 +0200

    climb stairs function ameliorated

--- blind75/climbStairs.ts
+++ blind75/climbStairs.ts
@@ -1,22 +1,7 @@
-function climbStairs(n: number): number {
-    // Memoization array to store the results of subproblems
-    const memo: number[] = [];
-
-    function climb(n: number): number {
-        // Base cases
-        if (n === 0 || n === 1) return 1;
-
-        // Check if we've already computed the result for this step
-        if (memo[n] !== undefined) return memo[n];
-
-        // Recursive relation: f(n) = f(n-1) + f(n-2)
-        memo[n] = climb(n - 1) + climb(n - 2);
-        
-        return memo[n];
-    }
-
-    return climb(n);
+function climbStairs(n: number, memo: number[] = []): number {
+    if (n === 0 || n === 1) return 1;
+    if (memo[n] !== undefined) return memo[n];
+    return memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
 }
 
-
-console.log(climbStairs(3));
+console.log(climbStairs(3))
