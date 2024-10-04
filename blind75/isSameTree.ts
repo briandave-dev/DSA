6531955e27e9096ec412e20128466aaf77c7d9e1
briandave-dev <briandave.dev@gmail.com>
2024-10-04 22:04:21 +0200

    is Same tree done

---
+++ blind75/isSameTree.ts
@@ -0,0 +1,12 @@
+import { TreeNode } from "../trees/tree";
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    if(!p && !q) return true;
+    if(!p || !q) return false;
+
+    if(p.val === q.val) {
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+    } else {
+        return false;
+    }
+};
--- blind75/isSubtree.ts
+++ blind75/isSubtree.ts
@@ -1,22 +1,24 @@
-import { TreeNode } from "../trees/tree";
+import { TreeNode } from "../trees/tree"
 
-function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
-    if (!subRoot) return true;
-    if (!root) return false;
+function isSubtree(root: TreeNode | null, subroot: TreeNode | null): boolean {
+    if(!subroot) return true;
+    if(!root) return false;
 
     function isSame(a: TreeNode | null, b: TreeNode | null): boolean {
-        if (!a && !b ) return true;
+        if(!a && !b) return true;
 
-        if (a && b && a.val === b.val) {
+        if(a && b && a.val === b.val){
             return isSame(a.left, b.left) && isSame(a.right, b.right);
-        } 
+        }
 
         return false;
     }
 
-    if (isSame(root, subRoot)) {
-        return true;
+    if(isSame(root, subroot)){
+        return false
     } else {
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        return isSubtree(root.left, subroot) || isSubtree(root.right, subroot);
     }
-};
+
+    return false;
+}
