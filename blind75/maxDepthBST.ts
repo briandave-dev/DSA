7a0dc30d4d723120054f1ddddcca93cd5f4e2112
briandave-dev <briandave.dev@gmail.com>
2024-10-05 21:42:41 +0200

    Max depth of a BST algorithm done

---
+++ blind75/maxDepthBST.ts
@@ -0,0 +1,12 @@
+import { TreeNode } from "../trees/tree";
+
+function maxDepth(root: TreeNode | null): number {
+    if (root === null) {
+        return 0;
+    }
+
+    let leftDepth = maxDepth(root.left);
+    let rightDepth = maxDepth(root.right);
+
+    return Math.max(leftDepth, rightDepth) + 1;
+}
